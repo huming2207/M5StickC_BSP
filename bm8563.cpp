16927dcbb509924116a40c2633b8e2244011dbca
Jackson Ming Hu <huming2207@gmail.com>
2019-08-25 22:32:41 +1000

    rtc: init

---
+++ bm8563.cpp
@@ -0,0 +1,131 @@
+#include "bm8563.hpp"
+
+uint8_t bm8563::get_sec()
+{
+    return 0;
+}
+
+void bm8563::set_sec(uint8_t sec)
+{
+
+}
+
+uint8_t bm8563::get_min()
+{
+    return 0;
+}
+
+void bm8563::set_min(uint8_t min)
+{
+
+}
+
+uint8_t bm8563::get_hour()
+{
+    return 0;
+}
+
+void bm8563::set_hour(uint8_t hour)
+{
+
+}
+
+uint8_t bm8563::get_day()
+{
+    return 0;
+}
+
+void bm8563::set_day(uint8_t day)
+{
+
+}
+
+uint8_t bm8563::get_dow()
+{
+    return 0;
+}
+
+void bm8563::set_dow(uint8_t dow)
+{
+
+}
+
+uint8_t bm8563::get_month()
+{
+    return 0;
+}
+
+void bm8563::set_month(uint8_t month)
+{
+
+}
+
+uint8_t bm8563::get_year()
+{
+    return 0;
+}
+
+void bm8563::set_year(uint8_t year)
+{
+
+}
+
+uint8_t bm8563::get_minute_alarm()
+{
+    return 0;
+}
+
+void bm8563::set_minute_alarm(uint8_t minute)
+{
+
+}
+
+void bm8563::enable_minute_alarm(bool enable)
+{
+
+}
+
+uint8_t bm8563::get_hour_alarm()
+{
+    return 0;
+}
+
+void bm8563::set_hour_alarm(uint8_t minute)
+{
+
+}
+
+void bm8563::enable_hour_alarm(bool enable)
+{
+
+}
+
+uint8_t bm8563::get_day_alarm()
+{
+    return 0;
+}
+
+void bm8563::set_day_alarm(uint8_t minute)
+{
+
+}
+
+void bm8563::enable_day_alarm(bool enable)
+{
+
+}
+
+uint8_t bm8563::get_dow_alarm()
+{
+    return 0;
+}
+
+void bm8563::set_dow_alarm(uint8_t minute)
+{
+
+}
+
+void bm8563::enable_dow_alarm(bool enable)
+{
+
+}
---
+++ include/bm8563.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstdint>
+
+class bm8563
+{
+    public:
+        uint8_t get_sec();
+        void set_sec(uint8_t sec);
+        uint8_t get_min();
+        void set_min(uint8_t min);
+        uint8_t get_hour();
+        void set_hour(uint8_t hour);
+        uint8_t get_day();
+        void set_day(uint8_t day);
+        uint8_t get_dow();
+        void set_dow(uint8_t dow);
+        uint8_t get_month();
+        void set_month(uint8_t month);
+        uint8_t get_year();
+        void set_year(uint8_t year);
+
+        uint8_t get_minute_alarm();
+        void set_minute_alarm(uint8_t minute);
+        void enable_minute_alarm(bool enable);
+        uint8_t get_hour_alarm();
+        void set_hour_alarm(uint8_t minute);
+        void enable_hour_alarm(bool enable);
+        uint8_t get_day_alarm();
+        void set_day_alarm(uint8_t minute);
+        void enable_day_alarm(bool enable);
+        uint8_t get_dow_alarm();
+        void set_dow_alarm(uint8_t minute);
+        void enable_dow_alarm(bool enable);
+
+    private:
+        bm8563() = default;
+
+};
